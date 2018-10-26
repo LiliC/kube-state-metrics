9fc266b60ca86e740f2f3be820e7463195e19555
Lili Cosic <cosiclili@gmail.com>
2018-10-26 12:45:18 +0200

    builder: add pkgs -

--- pkg/collectors/builder.go
+++ pkg/collectors/builder.go
@@ -18,6 +18,7 @@ limitations under the License.
 package collectors
 
 import (
+	"fmt"
 	"strings"
 
 	autoscaling "k8s.io/api/autoscaling/v2beta1"
@@ -29,9 +30,14 @@ import (
 	"golang.org/x/net/context"
 	"k8s.io/api/core/v1"
 	"k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/dynamic"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/kube-state-metrics/pkg/metrics"
 	metricsstore "k8s.io/kube-state-metrics/pkg/metrics_store"
 	"k8s.io/kube-state-metrics/pkg/options"
